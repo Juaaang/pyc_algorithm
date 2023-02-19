9fa52d97b0d2cb3b534a70e606ef19e61052a344
Juaaang <au0502@naver.com>
2023-02-20 07:10:47 +0900

    1

--- 10760.py
+++ 10760.py
@@ -10,18 +10,21 @@ T = int(input())
 for tc in range(1,T+1):
     N, M = map(int, input().split())
     arr = [list(map(int, input().split())) for _ in range(N)]
-    row = 0
-    col = 0
-    cnt = 0
     dr = [0, 1, 1, 1, 0, -1, -1, -1]
     dc = [1, 1, 0, -1, -1, -1, 0, 1]
     d = 0
-    while row < N:
-        newR = row + dr[d]
-        newC = col + dc[d]
-        if 0 <= newR < N and 0 <= newC < N and arr[row][col] > arr[newR][newC]:
-            d = (d + 1) % 8
-            cnt += 1
-            continue
-        d = (d + 1) % 8
+    res = 0
+    for i in range(N):
+        for j in range(M):
+            cnt = 0
+            for d in range(8):
+                newR = i + dr[d]
+                newC = j + dc[d]
+                if 0<= newR < N and 0<= newC < M and arr[newR][newC] < arr[i][j]:
+                    cnt += 1
+            if cnt >= 4:
+                res += 1
+    print(f'#{tc} {res}')
+
+
 
