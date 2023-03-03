4eb7c62e2b67d742afc58150e682cf812a4c07c7
Juaaang <Juaaang@github.com>
2023-03-03 16:47:41 +0900

    3.3

--- 5356.py
+++ 5356.py
@@ -9,7 +9,7 @@ sys.stdin = open('input.txt', 'r')
 T = int(input())
 for tc in range(1,T+1):
     board = [list(input().split()) for _ in range(5)]
+    new_str = ''
     for j in range(5):
-        new_str = ''
         for i in range(5):
             new_str += board[i][j]
